be2419120c4d55189c73456a43a501bcde8e2cb2
SocratesCV <socracv@gmail.com>
2023-01-20 21:39:10 +0100

    Limpieza del scrip, añadida una pausa para ver la imformación que se muestra por pantalla.

--- imInstagram.sh
+++ imInstagram.sh
@@ -11,9 +11,6 @@ FINAL="imagenFinal.png"
 INTERMEDIO="imagenConBorde.png"
 
 
-
-display $1
-
 echo "  "\n
 echo "-------------------------------------------------"
 echo Cambiando la imagen $1 para Instagram.
@@ -21,13 +18,12 @@ echo le daremos un margen de un $INCREMENTO %
 echo con el color de fondo $COLOR
 echo "-------------------------------------------------"
 echo " "
+sleep 3
 
 convert $1 -bordercolor white -border 1 \
 	-bordercolor grey60 -border 1 \
 	\( +clone -background black -shadow 60x4+4+4 \) +swap \
 	-background '#288368' -mosaic $INTERMEDIO 
-
-display $INTERMEDIO
 
 # Vemos los parámetros de la imagen que le hemos pasado.
 ancho=$(identify -format %w $INTERMEDIO )
@@ -41,12 +37,6 @@ else
 	maxValor=$alto
 fi
 
-echo "Ancho $ancho"
-echo "Alto $alto"
-
-echo "Cuadramos la imagen a $maxValor"
-
-echo "Un incremento de $INCREMENTO % de $maxValor es " 
 valorFinal=$(expr $maxValor \* $INCREMENTO / 100 + $maxValor)
 echo $valorFinal
 
@@ -54,10 +44,11 @@ echo $valorFinal
 convert $INTERMEDIO -gravity center -background '#288368' -extent "$valorFinal"x"$valorFinal" $FINAL
 
 # Movemos el archivo original
-# mv $1 ./originales
+mv $1 ./originales
 
-
-# convert prueba.png \( +clone  -background "288368" -shadow 80x3+5+5 \) +swap \              -background none   -layers merge  +repage   prueba2.png
 # mostramos el archivo
 # Cerramos la ventana con "q"
 display $FINAL
+
+# Fin del archivo.
+#-----------------
