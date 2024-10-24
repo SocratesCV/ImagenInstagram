25b44bc90e8d5a46524c4e9887113147e397ef88
Socrates <socracv@gmail.com>
2024-10-24 22:13:22 +0200

    En el archivo README.md he añadido un montón de nuevas ideas y opciones que me gustaría añadir. En imInstagram.sh he comentado las líneas correspondientes a la descarga de la imagen con instaloader. También añadido la con probación de errores, gracias la ia del editor "cursor".

--- README.md
+++ README.md
@@ -6,18 +6,24 @@ Se le pasa al scrip el nombre de la imagen a modificar y lo modifica directament
 
 
 ## Mejoras realizadas
-Ahora pone un borde sombreado y despues redimensiona la imagen.
+[x] Ahora pone un borde sombreado y despues redimensiona la imagen.
 
 
 ## Problemas
-Ahora va mostrando la imagen según se va modificando. Todo esto hay que quitarlo.
+[ ] Ahora va mostrando la imagen según se va modificando. Todo esto hay que quitarlo.
 
 ## Mejoras futuras.
-Limpiar el script. Quitar las visualizaciones intermedias.
-Preguntar si todo ha salido bien y elimina el archivo intermedio y mueve el archivo original.
-Mejorar este archivo README.md
-Ver si puedo ver el archivo de imagen directamente en la terminal (solo si es kity)
+[ ] Limpiar el script. Quitar las visualizaciones intermedias.
+[ ] Preguntar si todo ha salido bien y elimina el archivo intermedio y mueve el archivo original.
+[ ] Mejorar este archivo README.md
+[ ] Ver si puedo ver el archivo de imagen directamente en la terminal (solo si es kity)
+[ ] Añadir a la imagen un logotipo de intagram con el nombre del creador de la publicación.
+[ ] Crear un logo de instagram con los colores del fondo que ponemos.
+[ ] Logo colocarse en una posición determinada, independientemente del tamaño, posiblemente ponerlo antes de dimensionar.
+[ ] El nombre del creador de la publicación con el mismo color del fondo.
+[ ] Tener una variante de color de fondo y texto por si no queda bien con la imagen.
 
+## Cosas de momento imposibles.
 Ver si puedo publicarlo directamente en instagram.
 	Añadir el texto
 	Añadirá directamente los hashtag
--- imInstagram.sh
+++ imInstagram.sh
@@ -8,30 +8,79 @@
 # un primer paso.
 #------------------------------------------------------------------------
 
-# CONSTANTES
+
+
+# CONSTANTES GLOBALES
 # Porcentaje de imcremento 
 INCREMENTO=10
 # Color de fondo
 COLOR=288368
+#imagen inicial que vamos a transformar
+INICIAL=comprobarParametros
 #imagen donde se guardará el resultado
 FINAL="imagenFinal.png"
 #imagen para guardar paso intermedio.
 INTERMEDIO="imagenConBorde.png"
 
+# Funciones...
+#
+# Función que verifica que se haya pasado  los argumentos correctos
+function comprobarParametros{ 
+  if [$# -eq 0]; then
+    echo "Debes proporcinar un número de imagen"
+    exit 1 
+  fi 
 
-echo "  "\n
-echo "-------------------------------------------------"
-echo Cambiando la imagen $1 para Instagram.
-echo le daremos un margen de un $INCREMENTO %
-echo con el color de fondo $COLOR
-echo "-------------------------------------------------"
-echo " "
-sleep 3
+  # Guardar el numero pasado como argumento
+  numero=$2 
 
-convert $1 -bordercolor white -border 1 \
-	-bordercolor grey60 -border 1 \
-	\( +clone -background black -shadow 60x4+4+4 \) +swap \
-	-background '#288368' -mosaic $INTERMEDIO 
+  # construimos el nombre del archivo
+  archivo="imagen${numero}.jpg"
+  return=$archivo
+ 
+}
+
+
+
+# Funcion con la que descargaremos una imagen de instagram utilizando instaloader()
+function descargaImagen(){
+  instaloader --dirname=dirimagen --filename=imagenInicial -- -$1 
+
+  # Verifica que el archivo exite
+  if [ -f "$archivo" ]; then
+    return=$archivo
+  else
+    echo "El archivo $archivo no exite."
+    exit 1 
+  fi 
+
+}
+
+# Funcion mensaje, muestra un mensaje por pantalla
+function mensajeInicial(){
+
+  echo "  "\n
+  echo "-------------------------------------------------"
+  echo Cambiando la imagen $1 para Instagram.
+  echo le daremos un margen de un $INCREMENTO %
+  echo con el color de fondo $COLOR
+  echo "-------------------------------------------------"
+  echo " "
+  sleep 3
+}
+
+# Funcion para crear un pequeño borde sombreado entorno a la imagen 
+function bordeSombreado{ 
+  convert ~/dirimagen/$INICIAL -bordercolor white -border 1 \
+  -bordercolor grey60 -border 1 \
+  \( +clone -background black -shadow 60x4+4+4 \) +swap \
+  v	-background '#288368' -mosaic $INTERMEDIO 
+}
+
+
+mensajeInicial
+descargaImagen
+bordeSombreado
 
 # Vemos los parámetros de la imagen que le hemos pasado.
 ancho=$(identify -format %w $INTERMEDIO )
